79f85f378066696f69dfc29cdf8a1d5b9ecb0f47
unknown <visdec12@gmail.com>
2023-06-16 12:07:00 +0530

    interface and abstract method

--- oops.php
+++ oops.php
@@ -1,5 +1,37 @@
 <?php
 
+interface car
+{
+    function color();
+    
+     const tree = 400;
+}
 
+interface truck {
+    function vehicle();
+}
+
+class toyota implements car,truck
+{
+    function vehicle(){
+        echo 'toyota engine';
+    }
+
+    function color(){
+        echo "toyota color is blue";
+    }
+}
+
+class toyotaNewModel extends toyota
+{
+    function engine()
+    {
+        echo "engine function";
+    }
+}
+
+
+$obj = new toyotaNewModel;
+echo $obj::tree;
 
 ?>
